823ef787e4cad7ae5117122e261a419fca95271c
Y2ktorrez <andrestorrezvaca@gmail.com>
2024-06-24 17:30:12 -0400

    pokedex

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
+
+
+@Module({
+  imports: [
+
+    //Levantar un Servidor Estatico
+    ServeStaticModule.forRoot({ 
+      rootPath: join(__dirname,'..','public'), 
+      }),
+    
+    //Nos conectamos a la Base de datos
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    
+    PokemonModule 
+  ],
+})
+export class AppModule {}
